7da27660b2f1ff21f7b3aedaebf3dc641527d555
wajdam39 <37147153+wajdam39@users.noreply.github.com>
2021-08-24 21:06:34 +0200

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from flask import Flask
 
 app = Flask(__name__)
 
-def say_hello(username = "Woorlld"):
+def say_hello(username = "Wooyrlld"):
     return '<p>Hello %s!</p>\n' % username
 
 # Some bits of text for the page
